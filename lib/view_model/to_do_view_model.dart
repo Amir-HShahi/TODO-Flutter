96b287267e3eda228ee2968fa69ccfa5a12f4e6a
Amir-HShahi <amir13831107h@gmail.com>
2024-08-15 12:02:37 +0330

    Add: - to_do_view_model.dart

---
+++ lib/view_model/to_do_view_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:todolist/model/to_do_model.dart';
+
+class ToDoViewModel with ChangeNotifier {
+  final List<ToDoModel> _toDoGroup = [];
+
+  List<ToDoModel> getToDoGroup() {
+    return _toDoGroup;
+  }
+
+  void addToDo(String text) {
+    _toDoGroup.add(ToDoModel(text: text));
+  }
+}
