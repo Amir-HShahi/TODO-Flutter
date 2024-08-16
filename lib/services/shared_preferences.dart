370bc5bac9c664ae9371e9b36683885c69469b65
Amir-HShahi <amir13831107h@gmail.com>
2024-08-16 18:49:41 +0330

    Shared preferences functions implemented

---
+++ lib/services/shared_preferences.dart
@@ -0,0 +1,16 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<SharedPreferences> getSharedPreferences() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs;
+}
+
+void saveKeyValue(String key, String value) async {
+  final prefs = await getSharedPreferences();
+  prefs.setString(key, value);
+}
+
+void saveListKeyValue(String key, List<String> value) async {
+  final prefs = await getSharedPreferences();
+  await prefs.setStringList(key, value);
+}
