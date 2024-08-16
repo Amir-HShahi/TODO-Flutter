f62d00e385f2d4fbd9d69a71d61dfcafdc1f9681
Amir-HShahi <amir13831107h@gmail.com>
2024-08-16 16:31:39 +0330

    Fixed: provider not found

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,6 +1,9 @@
 import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 import 'package:todolist/view_model/to_do_view_model.dart';
+import 'package:todolist/view_model/user_info_view_model.dart';
 
-var providersData = [
+List<SingleChildWidget> providersData = [
   ChangeNotifierProvider(create: (_) => ToDoViewModel()),
+  ChangeNotifierProvider(create: (_) => UserInfoViewModel())
 ];
